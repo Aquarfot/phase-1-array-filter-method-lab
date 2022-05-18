27e4803abb076752b0f371b9ee19fb793de0f157
Andrew Quarfot <andrew.quarfot@gmail.com>
2022-05-18 16:39:00 -0600

    commit tree 74b6fba1a5b132bfd03ca2590bd999fcd1314bc3

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 // Code your solution here
+function findMatching(namesArr, string) {
+    return namesArr.filter((possibleName) => possibleName.toLowerCase() === string.toLowerCase()
+    );
+}
+
+function fuzzyMatch(source, testString) {
+    return source.filter((possibleName) => possibleName.toLowerCase().indexOf(testString.toLowerCase()) === 0
+    )
+};
+
+function matchName(source, matchingName) {
+    return source.filter((record) => record.name === matchingName);
+}
